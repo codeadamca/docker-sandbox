882a0a5cd217cc3ee627339203b3ca93d0524619
Adam <thomasadam83@hotmail.com>
2024-02-16 11:21:23 -0500

    Updated Docker config

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 
-phpinfo();
+$connect = mysqli_connect(
+    'db',
+    'demo',
+    'password',
+    'demo'
+);
+
+$query = 'SELECT *
+    FROM test';
+$result = mysqli_query($connect, $query);
+
+echo '<h1>MySQL Content:</h1>';
+
+while($record = mysqli_fetch_assoc($result))
+{
+    echo '<h2>'.$record['something'].'</h2>';
+    echo '<hr>';
+}
+
+
+
+// phpinfo();
+
