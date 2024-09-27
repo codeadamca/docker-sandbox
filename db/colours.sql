f6e4b7c491859570378659a2943a353f5595318c
Adam <thomasadam83@hotmail.com>
2024-09-27 08:10:26 -0400

    Docker review

--- db/colours.sql
+++ db/colours.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: db:3306
--- Generation Time: Jun 12, 2024 at 05:03 PM
+-- Host: db
+-- Generation Time: Jun 12, 2024 at 08:44 PM
 -- Server version: 8.0.33
 -- PHP Version: 8.2.15
 
@@ -37,10 +37,10 @@ CREATE TABLE `colours` (
 --
 
 INSERT INTO `colours` (`id`, `name`) VALUES
-(1, 'red'),
-(2, 'blue'),
-(3, 'green'),
-(4, 'purple');
+(1, 'Red'),
+(2, 'Blue'),
+(3, 'Green'),
+(4, 'Purple');
 
 --
 -- Indexes for dumped tables
--- www/index.php
+++ www/index.php
@@ -1,14 +1,13 @@
 <?php
 
 $connect = mysqli_connect(
-    'db',
-    'demo',
+    'db', 
+    'demo', 
     'password'
 );
 mysqli_select_db($connect, 'demo');
 
 $query = 'SELECT * FROM colours';
-
 $result = mysqli_query($connect, $query);
 
 while($record = mysqli_fetch_assoc($result))
@@ -17,4 +16,3 @@ while($record = mysqli_fetch_assoc($result))
     print_r($record);
     echo '</pre>';
 }
-
